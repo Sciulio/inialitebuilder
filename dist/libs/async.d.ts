d82b3d5485ad00b4cd343a474072846bb40585d4
Sciulio <sciulio@msn.com>
2018-08-01 18:03:48 +0200

    improved array async helper

--- dist/libs/async.d.ts
+++ dist/libs/async.d.ts
@@ -1,5 +1,7 @@
+declare type tCatchHandler<T> = (err: Error, item: T, idx: number) => void;
+declare type tDefaultValueOrCatchHandler<T, W> = W | ((err: Error, item: T, idx: number) => W);
 interface Array<T> {
-    mapAsync<W>(cback: (item: T, idx: number) => Promise<W>): Promise<W[]>;
-    forEachAsync(cback: (item: T, idx: number) => Promise<void>): Promise<void>;
-    filterAsync(cback: (item: T, idx: number) => Promise<boolean>): Promise<T[]>;
+    mapAsync<W>(cback: (item: T, idx: number) => Promise<W>, defaultValueOrCatchHandler?: tDefaultValueOrCatchHandler<T, W>): Promise<W[]>;
+    forEachAsync(cback: (item: T, idx: number) => Promise<void>, catchHandler?: tCatchHandler<T>): Promise<void>;
+    filterAsync(cback: (item: T, idx: number) => Promise<boolean>, defaultValueOrCatchHandler?: tDefaultValueOrCatchHandler<T, boolean>): Promise<T[]>;
 }
--- dist/libs/async.js
+++ dist/libs/async.js
@@ -7,25 +7,56 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Array.prototype.mapAsync = function (cback) {
+Array.prototype.mapAsync = function (cback, defaultValueOrCatchHandler) {
     return __awaiter(this, void 0, void 0, function* () {
-        return yield Promise.all(this.map((item, idx) => __awaiter(this, void 0, void 0, function* () { return yield cback(item, idx); })));
+        if (defaultValueOrCatchHandler) {
+            return yield Promise.all(this.map((item, idx) => __awaiter(this, void 0, void 0, function* () {
+                try {
+                    return yield cback(item, idx);
+                }
+                catch (e) {
+                    if (typeof defaultValueOrCatchHandler == "function") {
+                        return defaultValueOrCatchHandler(e, item, idx);
+                    }
+                }
+                return defaultValueOrCatchHandler;
+            })));
+        }
+        else {
+            //return await Promise.all(this.map(async (item, idx) => await cback(item, idx)));
+            return yield Promise.all(this.map(yield cback));
+        }
     });
 };
-Array.prototype.forEachAsync = function (cback) {
+Array.prototype.forEachAsync = function (cback, catchHandler) {
     return __awaiter(this, void 0, void 0, function* () {
-        yield this.mapAsync(cback);
+        yield this.mapAsync(cback, catchHandler);
     });
 };
-Array.prototype.filterAsync = function (cback) {
+Array.prototype.filterAsync = function (cback, defaultValueOrCatchHandler) {
     return __awaiter(this, void 0, void 0, function* () {
         const clone = this.slice(0);
         const indices = [];
-        yield this.mapAsync((item, idx) => __awaiter(this, void 0, void 0, function* () {
-            if (!(yield cback(item, idx))) {
-                indices.push(idx);
-            }
-        }));
+        if (defaultValueOrCatchHandler) {
+            yield this.mapAsync((item, idx) => __awaiter(this, void 0, void 0, function* () {
+                if (!(yield cback(item, idx))) {
+                    indices.push(idx);
+                }
+            }), (err, item, idx) => {
+                if (typeof defaultValueOrCatchHandler == "function" ?
+                    defaultValueOrCatchHandler(err, item, idx) :
+                    defaultValueOrCatchHandler) {
+                    indices.push(idx);
+                }
+            });
+        }
+        else {
+            yield this.mapAsync((item, idx) => __awaiter(this, void 0, void 0, function* () {
+                if (!(yield cback(item, idx))) {
+                    indices.push(idx);
+                }
+            }));
+        }
         return clone.filter((item, idx) => !indices.some(id => id == idx));
     });
 };
